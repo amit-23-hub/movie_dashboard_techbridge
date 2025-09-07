e8dcf7c968dc9624a6f40fcf6575a71dad6ee742
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2025-09-07 22:39:32 +0530

    corse integration

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173' ,'http://localhost:5174'],
+  origin: ['https://movie-client-omega.vercel.app' ,'https://movie-admin-olive.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
